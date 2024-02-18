e823233b293390c54ddbe6fe11759ecc69553faa
dimash-git <dimash9i@yandex.com>
2024-02-18 16:00:11 +0600

    fix: range slider

--- resources/js/Components/range-slider.jsx
+++ resources/js/Components/range-slider.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 
 const RangeSlider = ({ min = 0, max = 100, step = 1, value = 0, onChange }) => {
+    const percentage = ((value - min) * 100) / (max - min);
+
     return (
-        <input
-            type="range"
-            min={min}
-            max={max}
-            step={step}
-            value={value}
-            onChange={onChange}
-            className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
-            style={{
-                backgroundSize: `${((value - min) * 100) / (max - min)}% 100%`,
-            }}
-        />
+        <div className="relative pt-5">
+            <div className="absolute left-0 top-0">{min}</div>
+            <div
+                className="absolute"
+                style={{ left: `calc(${percentage}% - 12px)`, top: "0px" }} // Adjust as necessary
+            >
+                {value}
+            </div>
+            <div className="absolute right-0 top-0">{max}</div>
+            <input
+                type="range"
+                min={min}
+                max={max}
+                step={step}
+                value={value}
+                onChange={onChange}
+                className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+                style={{
+                    backgroundSize: `${percentage}% 100%`,
+                }}
+            />
+        </div>
     );
 };
 
--- resources/js/Pages/Dashboard/Index.jsx
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -15,7 +15,7 @@ export default function Dashboard({ auth }) {
             <div className="flex flex-col gap-y-8 mt-8">
                 <DateTime />
                 <SectionNavList />
-                <DeviceList listClass="md:grid-flow-col md:auto-cols-[150px] max-md:grid-cols-2" />
+                {/* <DeviceList listClass="md:grid-flow-col md:auto-cols-[150px] max-md:grid-cols-2" /> */}
                 {/* listClass опциональный props для этого компонента, подробнее в самом компоненте */}
                 {/* md:auto-cols-[150px] начиная с medium сайз экранов каждая ячейка должна быть шириной 150px */}
                 {/* max-md:grid-cols-2 для девайсов меньше medium сайз экранов в 1 строке разместить по 2 элемента */}
